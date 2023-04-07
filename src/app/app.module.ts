6c3a789908bf6bf87c42c70031795b8c9bc909ef
Las04ka <denkostyuk@gmail.com>
2023-04-07 20:24:38 +0300

    created nav bar and header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, HeaderComponent, NavBarComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MatButtonModule, MatIconModule],
   providers: [],
   bootstrap: [AppComponent],
 })
